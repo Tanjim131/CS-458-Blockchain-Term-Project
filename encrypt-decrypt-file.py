cbeec0ab5a37cc2f7132b5baaab50073644e61e1
Zarin Tasnim Promi <zarinpromi003@gmail.com>
2023-05-03 21:01:44 -0600

    Corrected version

--- encrypt-decrypt-file.py
+++ encrypt-decrypt-file.py
@@ -1,197 +1,69 @@
+import os
+import sys
+from cryptography.fernet import Fernet
 
-pragma solidity ^0.8.0;
-import "hardhat/console.sol";
+def get_file_extension(filename):
+    _, file_extension = os.path.splitext(filename)
+    # remove the dot from the extension
+    file_extension = file_extension[1:]
 
+    return file_extension
 
-contract practitioner{
+def encrypt_file(filename, key):
+    """
+    Given a filename (str) and key (bytes), it encrypts the file and write it
+    """
+    f = Fernet(key)
 
-    enum UserType { Patient, Practitioner }
-    enum AccessType { Read, Write }
+    with open(filename, "rb") as file:
+        # read all file data
+        file_data = file.read()
 
-    //Custom Errors
-    error DoNotHaveAccess();
+    encrypted_data = f.encrypt(file_data)
 
+    with open(f"encrypted.{get_file_extension(filename)}", "wb") as f:
+        f.write(encrypted_data)
 
-    //personal info structure
-    struct PersonalInfo{
-        string  name;
-        string  dateOfBirth;
-        string  email;
-        address addrss;
-        bytes  publicKey;
-    }
+    return encrypted_data.decode('UTF-8')
 
-    struct PatientInfo{
-        PersonalInfo personalInfo;
-        bool isRecStored;
-    }
+def decrypt_file(encrypted_data, key, file_extesion):
+    """
+    Given a filename (str) and key (bytes), it decrypts the file and write it
+    """
+    f = Fernet(key)
 
-    struct PractitionerInfo{
-        PersonalInfo personalInfo;
-        string instituteName;
-        string designation;
-    }
+    decrypted_data = f.decrypt(encrypted_data)
 
+    # write the original file
+    with open(f"decrypt.{file_extesion}", "wb") as file:
+        file.write(decrypted_data)
 
-    mapping(address => bytes32) private patientRecHash;
+    return decrypted_data.decode('UTF-8')
 
-    // total count
-    mapping(address=> PatientInfo ) private allPatientList;
-    mapping(address=> PractitionerInfo ) private allPractitionertList;
-    address[] private idListPatient;
-    address[] private idListPractitioner;
+# uncomment this if it's the first time you run the code, to generate the key
+# write_key()
+# load the key
+# key = load_key("E7-6xpblDveex3HPV4KN-uFqZgoYHHDJU2YYhlQj9ZA=")
+# file name
+# file = "sample.pdf"
+# encrypt it
+# encrypted_data = encrypt_file(file, key)
+# decrypt_file(encrypted_data, key)
 
-    //patient end counts
-    mapping(address=> mapping(address => PractitionerInfo) ) private authorizedPractitionersMap;
-    mapping(address=> mapping(address => PractitionerInfo) ) private pendingReqPtractitionersMap;
-    //mapping(address => address[]) private authorizedPractitioners;
-    //mapping(address => address[]) private pendingReqPtractitioners;
+if __name__ == "__main__":
+    function_name = sys.argv[1]
+    file_name = sys.argv[2]
+    key = sys.argv[3]
 
-    //practitioner end counts
-    mapping(address=> mapping(address => PatientInfo) ) private accessiblePatientsMap;
-    //mapping(address => address[]) private accessiblePatients;
+    if len(sys.argv) == 5:
+        file_extesion = sys.argv[4]
 
-    //generate Id
-    function generateId(bytes memory data) internal returns (bytes32) {
-        return keccak256(abi.encodePacked(block.difficulty , block.timestamp ,data));
-    }
+    key = bytes(key, 'UTF-8')
 
-    function checkHash(bytes32 hash, address patient) public returns (bool){
-        require(allPatientList[patient].personalInfo.addrss 
-            != address(0), "Not a Valid patient");
+    if function_name == "encrypt_file":
+        decrypted_file_content = encrypt_file(file_name, key)
+        print(decrypted_file_content)
 
-        if(patientRecHash[patient] == hash){
-            return true;
-        }
-        return false;
-    }
-    function savePatientHash(bytes32 hash, address patient) public {
-        require(allPatientList[patient].personalInfo.addrss 
-            != address(0), "Not a Valid patient");
-
-        patientRecHash[patient] = hash;
-    }
-
-
-    //introduce patient in chain
-    function createPatient(string memory name, string memory dateOfBirth, string memory email, bytes memory publicKey, address addrss )public returns (PatientInfo memory){
-
-        PatientInfo memory patientInfo = PatientInfo(PersonalInfo(name, dateOfBirth, email, addrss, publicKey), false);
-
-        allPatientList[addrss] = patientInfo;
-        idListPatient.push(addrss);
-        console.log(msg.sender);
-        return patientInfo;
-    }
-
-    //introduce practitioner in chain
-    function createPractitioner(string memory name, string memory dateOfBirth, string memory email, 
-            string memory instituteName, string memory designation, bytes memory publicKey, address addrss) private returns (PractitionerInfo memory){
-        
-        PractitionerInfo memory practitionerInfo = 
-        PractitionerInfo(PersonalInfo(name, dateOfBirth, email, addrss, publicKey), instituteName, designation);
-
-        allPractitionertList[addrss] = practitionerInfo;
-
-        idListPractitioner.push(addrss);
-        return practitionerInfo;
-    }
-
-
-    // Check if practitioner has access
-    function isAuthorized(address practitionerAddr, address patientId) public view returns(bool) {
-        // Check if practitioner id is valid
-        if(allPractitionertList[practitionerAddr].personalInfo.addrss != address(0)){
-            // Check if patient has authorized that practitioner
-            if(authorizedPractitionersMap[patientId][practitionerAddr].personalInfo.addrss 
-            != address(0)){
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    function authorizeUser(address patractitioner, address patient)  public  {
-        deleteFromPendingList(patractitioner, patient);
-        authorizedPractitionersMap[patient][patractitioner] = allPractitionertList[patractitioner];
-    }
-
-    function denyUser(address patractitioner, address patient)  public  {
-        deleteFromPendingList(patractitioner, patient);
-    }
-
-    function deleteFromPendingList(address practitioner, address patient) public {
-        delete pendingReqPtractitionersMap[patient][practitioner]; 
-    }
-    
-    function revokeAccess(address practitioner, address patient) public {
-        delete authorizedPractitionersMap[patient][practitioner];
-    }
-
-    
-    function getPendingUserList(address patient) public view returns( PractitionerInfo[] memory) {
-       uint256 len = idListPractitioner.length;
-        PractitionerInfo[] memory practitioners = new PractitionerInfo[](len);
-
-        for (uint256 i = 0; i < len; i++) {
-            address practitioner = idListPractitioner[i];
-            if (pendingReqPtractitionersMap[patient][practitioner].personalInfo.addrss != address(0)) {
-            practitioners[i] = pendingReqPtractitionersMap[patient][practitioner];
-            }
-        }
-
-        return (practitioners);
-    }
-
-    function getAuthUserList(address patient) public view returns( PractitionerInfo[] memory) {
-        uint256 len = idListPractitioner.length;
-        PractitionerInfo[] memory practitioners = new PractitionerInfo[](len);
-
-        for (uint256 i = 0; i < len; i++) {
-            address practitioner = idListPractitioner[i];
-            if (authorizedPractitionersMap[patient][practitioner].personalInfo.addrss != address(0)) {
-            practitioners[i] = authorizedPractitionersMap[patient][practitioner];
-            }
-        }
-
-        return (practitioners);
-    }
-    
-    //get authorized patients 
-    function getAccessiblePatientList(address practitioner) public view returns(PatientInfo[] memory) {
-        uint256 len = idListPatient.length;
-        PatientInfo[] memory patientInfos = new PatientInfo[](len);
-
-        for (uint256 i = 0; i < len; i++) {
-            address patient = idListPatient[i];
-            if (accessiblePatientsMap[practitioner][patient].personalInfo.addrss != address(0)) {
-            patientInfos[i] = accessiblePatientsMap[practitioner][patient];
-            }
-        }
-
-        return (patientInfos);
-    }
-    
-
-    //get all existing patient 
-    function getAllPatientList(address practitioner) public view returns(PatientInfo[] memory) {
-        uint256 len = idListPatient.length;
-        PatientInfo[] memory patientInfos = new PatientInfo[](len);
-
-        for (uint256 i = 0; i < len; i++) {
-            address patient = idListPatient[i];
-            if (accessiblePatientsMap[practitioner][patient].personalInfo.addrss != address(0)) {
-            patientInfos[i] = accessiblePatientsMap[practitioner][patient];
-            }
-        }
-
-        return (patientInfos);
-    }
-
-
-    function requestPermission(address patient, address practitioner) private  {
-        pendingReqPtractitionersMap[patient][practitioner] = allPractitionertList[practitioner] ;
-    }
-
-
-}
+    if function_name == "decrypt_file":
+        decrypted_file_content = decrypt_file(file_name, key, file_extesion)
+        print(decrypted_file_content)
